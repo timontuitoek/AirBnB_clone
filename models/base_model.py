5a7ed16df9056e206d52557c267692d2c887ec6c
timontuitoek <tuitoektimon@gmail.com>
2023-10-10 14:47:59 -0700

    Updated base models/base_model.py using **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,27 @@ class BaseModel:
     """
     parent class instance
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         instantation
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = str_to_datetime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        setattr(self, key, value)
+                        if 'created_at' not in kwargs:
+                            self.created_at = datetime.now()
+                        if 'updated_at' not in kwargs:
+                            self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def str_to_datetime(date_string, format='%Y-%m-%dT%H:%M:%S.%f'):
+        return datetime.strptime(date_string, format)
 
     def save(self):
         """
