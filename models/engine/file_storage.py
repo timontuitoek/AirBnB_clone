86e383d38cb02ad4b411ed1da73378861cfebc3b
254guru <otienooluda@gmail.com>
2023-10-11 02:46:11 -0700

    file storage module implementation

---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+import module
+"""
+import json
+from models.base_model import BaseModel
+
+
+
+class FileStorage:
+    """
+    parent class module that assigns storage
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        data = {}
+        for key, value in FileStorage.__objects.items():
+            data[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(data, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    cls = class_name_to_cls[class_name]
+                    instance = cls(**value)
+                    self.new(instance)
+        except FileNotFoundError:
+            pass
