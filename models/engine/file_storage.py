f25437eb1b112a4cb144ccfb5004713c17cf383b
254guru <otienooluda@gmail.com>
2023-10-14 10:31:35 -0700

    add import modules

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,8 @@ from models.state import State
 from models.city import City
 from models.place import Place
 from models.review import Review
+from models.amenity import Amenity
+import models
 
 
 class FileStorage:
