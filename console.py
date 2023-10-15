6785ccb3f2bce791b5fd4f9d440bd2237dc57052
timontuitoek <tuitoektimon@gmail.com>
2023-10-15 14:17:18 -0700

    new-updates

--- console.py
+++ console.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-console
+Console DOC
 """
-
 import cmd
 import re
-from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -13,184 +11,280 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """
-    Console DOC
+    Class Doc
     """
     prompt = "(hbnb) "
-    allowed_classes = [
-            "BaseModel",
-            "User",
-            "State",
-            "City",
-            "Amenity",
-            "Place",
-            "Review"
-            ]
 
     def do_quit(self, args):
         """
-        DOC
+        do quit
         """
         return True
 
     def do_EOF(self, args):
         """
-        DOC
+        do EOF
         """
         return True
 
     def emptyline(self):
         """
-        DOC
+        empty line
         """
         pass
 
-    def do_create(self, line):
+    def do_create(self, arg):
         """
-        DOC
+        do create
         """
-        if not line:
-            print("** class name missing **")
-            return
-        class_name = line.split()[0]
-        if class_name not in self.allowed_classes:
-            print("** class doesn't exist **")
-            return
-        obj = eval(class_name)()
-        obj.save()
-        print(obj.id)
-
-    def do_show(self, line):
-        """
-        DOC
-        """
-        self.show_or_destroy(line, "show")
-
-    def do_destroy(self, line):
-        """
-        DOC
-        """
-        self.show_or_destroy(line, "destroy")
-
-    def show_or_destroy(self, line, action):
-        """
-        Helper method for showing or destroying an object
-        """
-        args = line.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in self.allowed_classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        instances = storage.all()
-        key = "{}.{}".format(class_name, args[1])
-        if key in instances:
-            obj = instances[key]
-            if action == "show":
-                print(obj)
-            else:  # action == "destroy"
-                del instances[key]
-                storage.save()
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                if args_array[0] == "BaseModel":
+                    obj = BaseModel()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "User":
+                    obj = User()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "State":
+                    obj = State()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "City":
+                    obj = City()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "Amenity":
+                    obj = Amenity()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "Place":
+                    obj = Place()
+                    obj.save()
+                    print(obj.id)
+                elif args_array[0] == "Review":
+                    obj = Review()
+                    obj.save()
+                    print(obj.id)
+                else:
+                    print("** class doesn't exist **")
         else:
-            print("** no instance found **")
+            print("** class name missing **")
 
-    def do_update(self, line):
+    def do_show(self, arg):
+        """
+        do show
+        """
+        allowed_classes = [
+                "BaseModel", "User", "State",
+                "City", "Amenity", "Place", "Review"]
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                class_name = args_array[0]
+                if class_name in allowed_classes:
+                    if len(args_array) > 1:
+                        objs_dict = models.storage.all()
+                        search_string = "{}.{}".format(
+                                class_name, args_array[1])
+                        if search_string in objs_dict:
+                            print(objs_dict[search_string])
+                        else:
+                            print("** no instance found **")
+                    else:
+                        print("** instance id missing **")
+                else:
+                    print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_destroy(self, arg):
+        """
+        do destroy
+        """
+        allowed_classes = [
+                "BaseModel", "User", "State",
+                "City", "Amenity", "Place", "Review"]
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                class_name = args_array[0]
+                if class_name in allowed_classes:
+                    if len(args_array) > 1:
+                        objs_dict = models.storage.all()
+                        search_string = "{}.{}".format(
+                                class_name, args_array[1])
+                        if search_string in objs_dict:
+                            del (objs_dict[search_string])
+                            models.storage.save()
+                        else:
+                            print("** no instance found **")
+                    else:
+                        print("** instance id missing **")
+                else:
+                    print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_all(self, arg):
+        """
+        do all
+        """
+        allowed_classes = [
+                "BaseModel", "User", "State",
+                "City", "Amenity", "Place", "Review"]
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                class_name = args_array[0]
+                if class_name in allowed_classes:
+                    final_list = []
+                    for key, value in models.storage.all().items():
+                        if (class_name in key):
+                            final_list.append(str(value))
+                    print(final_list)
+                else:
+                    print("** class doesn't exist **")
+        else:
+            final_list = []
+            for key, value in models.storage.all().items():
+                final_list.append(str(value))
+            print(final_list)
+
+    def do_update(self, arg):
         """
         do updates
         """
-        args = line.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in self.allowed_classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        instances = storage.all()
-        key = "{}.{}".format(class_name, args[1])
-        if key not in instances:
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        obj = instances[key]
-        setattr(obj, args[2], args[3].strip('\"'))
-        obj.save()
-
-    def do_User(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "User")
-
-    def do_BaseModel(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "BaseModel")
-
-    def do_State(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "State")
-
-    def do_City(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "City")
-
-    def do_Amenity(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "Amenity")
-
-    def do_Place(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "Place")
-
-    def do_Review(self, line):
-        """
-        DOC
-        """
-        self.handle_class_commands(line, "Review")
-
-    def handle_class_commands(self, line, class_name):
-        """
-        Helper method for handling class-specific commands
-        """
-        class_commands = {
-            ".all()": self.do_all,
-            ".count()": self.get_count,
-        }
-        args = line.split()
-        if args and args[0] in class_commands:
-            class_commands[args[0]](class_name)
+        allowed_classes = [
+                "BaseModel", "User", "State",
+                "City", "Amenity", "Place", "Review"]
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                class_name = args_array[0]
+                if class_name in allowed_classes:
+                    if len(args_array) > 1:
+                        objs_dict = models.storage.all()
+                        search_string = "{}.{}".format(
+                                class_name, args_array[1])
+                        if search_string in objs_dict:
+                            if len(args_array) > 2:
+                                if len(args_array) > 3:
+                                    if (args_array[3]
+                                            not in
+                                            ["created_at",
+                                                "updated_at", "id"]):
+                                        setattr(objs_dict[search_string], str(
+                                            args_array[2]), str(args_array[3]))
+                                else:
+                                    print("** value missing **")
+                            else:
+                                print("** attribute name missing **")
+                        else:
+                            print("** no instance found **")
+                    else:
+                        print("** instance id missing **")
+                else:
+                    print("** class doesn't exist **")
         else:
-            print("** invalid command **")
+            print("** class name missing **")
+
+    def do_User(self, arg):
+        """
+        do user
+        """
+        class_name = "User"
+        self.data_model_func(arg, class_name)
+
+    def do_BaseModel(self, arg):
+        """
+        do base models
+        """
+        class_name = "BaseModel"
+        self.data_model_func(arg, class_name)
+
+    def do_State(self, arg):
+        """
+        do state
+        """
+        class_name = "State"
+        self.data_model_func(arg, class_name)
+
+    def do_City(self, arg):
+        """
+        do city
+        """
+        class_name = "City"
+        self.data_model_func(arg, class_name)
+
+    def do_Amenity(self, arg):
+        """
+        do amenity
+        """
+        class_name = "Amenity"
+        self.data_model_func(arg, class_name)
+
+    def do_Place(self, arg):
+        """
+        do place
+        """
+        class_name = "Place"
+        self.data_model_func(arg, class_name)
+
+    def do_Review(self, arg):
+        """
+        do review
+        """
+        class_name = "Review"
+        self.data_model_func(arg, class_name)
+
+    def data_model_func(self, arg, class_name):
+        """
+        data models funct
+        """
+        allowed_methods = [".all()", ".count()"]
+        show_regex = re.compile(r"\.show\(\"(.*?)\"\)")
+        delete_regex = re.compile(r"\.destroy\(\"(.*?)\"\)")
+        update_regex = re.compile(r"\.update\(\"(.*?)\", \"(.*?)\", (.*?)\)")
+        update_dict_regex = re.compile(r"\.update\(\"(.*?)\",(.*?)\)")
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                command_method = args_array[0]
+                if command_method in allowed_methods:
+                    if command_method == ".all()":
+                        self.do_all(class_name)
+                    if command_method == ".count()":
+                        self.get_count(class_name)
+                elif (show_regex.search(args_array[0]) is not None):
+                    obj_id = show_regex.search(args_array[0]).group(1)
+                    self.do_show("{} {}".format(class_name, obj_id))
+                elif (delete_regex.search(args_array[0]) is not None):
+                    obj_id = delete_regex.search(args_array[0]).group(1)
+                    self.do_destroy("{} {}".format(class_name, obj_id))
+                elif (update_regex.search(arg) is not None):
+                    obj_id = update_regex.search(arg).group(1)
+                    obj_attr_name = update_regex.search(arg).group(2)
+                    obj_attr_value = update_regex.search(arg).group(3)
+                    self.do_update("{} {} {} {}".format(
+                        class_name, obj_id, obj_attr_name, obj_attr_value))
+                elif (update_dict_regex.search(arg) is not None):
+                    obj_id = update_dict_regex.search(arg).group(1)
+                    obj_dict = eval(update_dict_regex.search(arg).group(2))
+                    for key, value in obj_dict.items():
+                        self.do_update("{} {} {} {}".format(
+                            class_name, obj_id, key, value))
 
     def get_count(self, class_name):
         """
-        Helper method for counting instances
+        get count
         """
         allowed_classes = [
                 "BaseModel", "User", "State",
